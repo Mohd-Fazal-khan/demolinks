5a3ed9f5c2ebaead63c70366b58fca0b1510c33c
Fazal Khan <mohdfazalkhan56@gmail.com>
2025-12-08 18:16:23 +0530

    fix

--- api/flyer.js
+++ api/flyer.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const { flyerId, productId } = req.query;
 
   const deepLink = `dealzup://flyer/${flyerId}/${productId}`;
-  const playStore = "https://play.google.com/store/apps/details?id=com.dealzup";
+  const playStore = "https://play.google.com/store/apps/details?id=com.demolink";
 
   res.setHeader("Content-Type", "text/html");
 
