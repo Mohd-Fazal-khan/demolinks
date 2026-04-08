b7f8744aa808c8968d09973210ac2283978d9303
Fazal Khan <124332727+Mohd-Fazal-khan@users.noreply.github.com>
2026-04-08 17:23:19 +0530

    Update offer.js

--- api/offer.js
+++ api/offer.js
@@ -1,3 +1,22 @@
+// export default function handler(req, res) {
+//   const { offerId } = req.query;
+
+//   const deepLink = `dealzup://offer/${offerId}`;
+//   const playStore = "https://play.google.com/store/apps/details?id=com.dealzupuser";
+
+//   res.setHeader("Content-Type", "text/html");
+
+//   res.end(`
+//     <html>
+//       <script>
+//         window.location = "${deepLink}";
+//         setTimeout(() => {
+//           window.location = "${playStore}";
+//         }, 1200);
+//       </script>
+//     </html>
+//   `);
+// }
 export default function handler(req, res) {
   const { offerId } = req.query;
 
@@ -8,12 +27,13 @@ export default function handler(req, res) {
 
   res.end(`
     <html>
-      <script>
-        window.location = "${deepLink}";
-        setTimeout(() => {
-          window.location = "${playStore}";
-        }, 1200);
-      </script>
+      <head><meta http-equiv="refresh" content="0; url=${deepLink}" /></head>
+      <body>
+        <script>
+          window.location = "${deepLink}";
+          setTimeout(() => { window.location = "${playStore}"; }, 800);
+        </script>
+      </body>
     </html>
   `);
 }
