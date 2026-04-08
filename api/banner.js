2b1633a3e3856d79f1041fe9f6c111d597ac1066
Fazal Khan <124332727+Mohd-Fazal-khan@users.noreply.github.com>
2026-04-08 19:09:49 +0530

    Create banner.js

---
+++ api/banner.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  const { bannerId } = req.query;
+
+  const deepLink = `dealzup://banner/${bannerId}`;
+  const playStore = "https://play.google.com/store/apps/details?id=com.dealzupuser";
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.end(`
+    <html>
+      <head><meta http-equiv="refresh" content="0; url=${deepLink}" /></head>
+      <body>
+        <script>
+          window.location = "${deepLink}";
+          setTimeout(() => { window.location = "${playStore}"; }, 1200);
+        </script>
+      </body>
+    </html>
+  `);
+}
