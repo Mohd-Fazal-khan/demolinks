8df225839b74c8d1fcd483a340630ab31d5442c9
Fazal Khan <124332727+Mohd-Fazal-khan@users.noreply.github.com>
2026-04-08 17:05:47 +0530

    Create offer.js

---
+++ api/offer.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+  const { offerId } = req.query;
+
+  const deepLink = `dealzup://offer/${offerId}`;
+  const playStore = "https://play.google.com/store/apps/details?id=com.dealzupuser";
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.end(`
+    <html>
+      <script>
+        window.location = "${deepLink}";
+        setTimeout(() => {
+          window.location = "${playStore}";
+        }, 1200);
+      </script>
+    </html>
+  `);
+}
